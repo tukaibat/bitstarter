65a0fd9897bbee3e2957dcfc7d44875e2ba4956b
Aditya Mondal <tukaibat@gmail.com>
2013-07-13 12:12:07 +0000

    fixed web js errors

--- web.js
+++ web.js
@@ -1,12 +1,12 @@
 var express = require('express');
 var fs = require('fs');
-var fileLength = buf.write(fs.readFileSync('./index.html', 'utf-8'));
-var buf = new Buffer(fileLength);	
+var file = fs.readFileSync('./index.html', 'utf-8');
+//var buf = new Buffer(fileLength);	
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(buf.toString('utf-8', 0, fileLength));
+  response.send(file);
 });
 
 var port = process.env.PORT || 5000;
